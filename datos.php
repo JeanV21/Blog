943ffe9b603a337ebd47f830c421b0a7cbc5c8fc
JeanV21 <50106949+JeanV21@users.noreply.github.com>
2019-05-01 07:42:51 -0500

    Diseño

---
+++ datos.php
@@ -0,0 +1,12 @@
+
+<?php
+extract($_GET);
+// MOSTRAR UN DATO:var_dump($_GET);//
+echo "Nombre son $nombre";
+echo "<br/>";
+echo "Apellido son $apellido";
+echo "<br/>";
+echo "Correo son $correo";
+echo "<br/>";
+echo "Fechanac son $fecha";
+echo "<br/>";
---
+++ interfaz.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+extract($_GET);
+?>
+<html>
+
+<meta charset="utf-8">
+<head>
+	<title>Blog</title>
+	<link rel="stylesheet" type="text/css" href="diseño/diseño.css">
+	</head>
+	<a href="index.html" >Menu</a>
+<body background="diseño/imagen.jpg ">
+	<table border="1" align="rigth">
+		<tr>
+	     <td>$_usu</td>
+		</tr>
+</table>
+
+</body>
+</html>
